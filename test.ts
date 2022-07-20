99df9e2b61eac4209b18307fd0b95bda702fbb5e
Jeff-Squared <109444537+Jeff-Squared@users.noreply.github.com>
2022-07-19 20:39:13 -0500

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,10 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+TFT.init()
+TFT.clearScreen(Color.Yellow)
+TFT.drawCircle(40, 40, 20, Color.Green)
+TFT.drawRectangle(45, 45, 50, 50, Color.Red)
+TFT.drawRectangle(55, 55, 6, 6, Color.White)
+TFT.drawRectangle(68, 68, 6, 6, Color.White)
+TFT.drawRectangle(81, 81, 6, 6, Color.White)
+basic.forever(function () {
+	
+})
