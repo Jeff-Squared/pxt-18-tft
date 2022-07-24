02e251daa4a2315833d569327aa51d3848edf3ad
Jeff-Squared <109444537+Jeff-Squared@users.noreply.github.com>
2022-07-24 10:43:30 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,8 @@
-/**
- * Colors
- */
 enum Color {
     //% block="Black"
     Black = 0x0000,
     //% block="LightGrey"
     LightGrey = 0xC618,
-    //% block="DarkGrey"
-    DarkGrey = 0x7BEF,
     //% block="Blue"
     Blue = 0x001F,
     //% block="Green"
@@ -21,28 +16,19 @@ enum Color {
     //% block="Yellow"
     Yellow = 0xFFE0,
     //% block="White"
-    White = 0xFFFF,
-    //% block="Orange"
-    Orange = 0xFD20,
-    //% block="Pink"
-    Pink = 0xF81F
+    White = 0xFFFF
 }
 
 /**
-  * RB-TFT1.8 Block 
-  */
-//% color="#669999" icon="\uf008" weight=120 block="1.8 TFT Display"
+* RB-TFT1.8 Block
+*/
+//% color="#669999" icon="\uf008" weight=110 block="1.8 TFT Display"
+
 namespace TFT {
-    // Display commands & constants
+
     let TFTWIDTH = 160
     let TFTHEIGHT = 128
-    let FONT_WIDTH = 6
-    let FONT_HEIGHT = 11
-    let FONT_PIXEL_CNT = (FONT_WIDTH * FONT_HEIGHT) * 2
 
-    /**
-     * TFT Commands
-     */
     enum TFTCommands {
         NOP = 0x00,
         SWRESET = 0x01,
@@ -73,26 +59,50 @@ namespace TFT {
         PWRSAV = 0xF6
     }
 
-    /*
-     * Initial TFT setup
+    /**
+     * Unicode representation
+     * The unicode table is split into seven parts because of memory size and array size limitations of the microbit
      */
+    let fontOne: number[] = [0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422,
+        0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422,
+        0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422]
+    let fontTwo: number[] = [0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422,
+        0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x0022d422, 0x00000000, 0x000002e0,
+        0x00018060, 0x00afabea, 0x00aed6ea, 0x01991133, 0x010556aa, 0x00000060]
+    let fontThree: number[] = [0x000045c0, 0x00003a20, 0x00051140, 0x00023880, 0x00002200, 0x00021080,
+        0x00000100, 0x00111110, 0x0007462e, 0x00087e40, 0x000956b9, 0x0005d629, 0x008fa54c, 0x009ad6b7,
+        0x008ada88, 0x00119531, 0x00aad6aa, 0x0022b6a2, 0x00000140, 0x00002a00]
+    let fontFour: number[] = [0x0008a880, 0x00052940, 0x00022a20, 0x0022d422, 0x00e4d62e, 0x000f14be,
+        0x000556bf, 0x0008c62e, 0x0007463f, 0x0008d6bf, 0x000094bf, 0x00cac62e, 0x000f909f, 0x000047f1,
+        0x0017c629, 0x0008a89f, 0x0008421f, 0x01f1105f, 0x01f4105f, 0x0007462e]
+    let fontFive: number[] = [0x000114bf, 0x000b6526, 0x010514bf, 0x0004d6b2, 0x0010fc21, 0x0007c20f,
+        0x00744107, 0x01f4111f, 0x000d909b, 0x00117041, 0x0008ceb9, 0x0008c7e0, 0x01041041, 0x000fc620,
+        0x00010440, 0x01084210, 0x00000820, 0x010f4a4c, 0x0004529f, 0x00094a4c]
+    let fontSix: number[] = [0x000fd288, 0x000956ae, 0x000097c4, 0x0007d6a2, 0x000c109f, 0x000003a0,
+        0x0006c200, 0x0008289f, 0x000841e0, 0x01e1105e, 0x000e085e, 0x00064a4c, 0x0002295e, 0x000f2944,
+        0x0001085c, 0x00012a90, 0x010a51e0, 0x010f420e, 0x00644106, 0x01e8221e]
+    let fontSeven: number[] = [0x00093192, 0x00222292, 0x00095b52, 0x0008fc80, 0x000003e0, 0x000013f1,
+        0x00841080, 0x0022d422];
+
+    /*
+    * Initial TFT setup
+    */
     //% block="Initialize TFT"
     //% weight=100
+
     export function init(): void {
         // set SPI frequency
         pins.spiFrequency(5000000)
 
-        //Toggle reset
+        //Toggle reset pin
         pins.digitalWritePin(DigitalPin.P8, 1)
         basic.pause(100)
         pins.digitalWritePin(DigitalPin.P8, 0)
         basic.pause(100)
         pins.digitalWritePin(DigitalPin.P8, 1)
 
-        // Frame rate control - normal mode
+        // Frame rate control 
         send(TFTCommands.FRMCTR1, [0x01, 0x2C, 0x2D])
-
-        // Frame rate control - idle mode
         send(TFTCommands.FRMCTR2, [0x01, 0x2C, 0x2D])
         send(TFTCommands.FRMCTR3, [0x01, 0x2C, 0x2D, 0x01, 0x2C, 0x2D])
 
@@ -123,130 +133,90 @@ namespace TFT {
     }
 
     //% block="Clear screen color: %color"
-    //% weight=90
+    //% weight=95
+
     export function clearScreen(color: Color = 0): void {
         drawRectangle(0, 0, TFTWIDTH + 2, TFTHEIGHT + 2, color)
     }
 
     /*
-    * Draw single pixel
-    */
+     * Draw single pixel
+     */
     //% block="Draw single pixel at x:%x|y:%y with color:%color"
-    //% x.min=1 x.max=162
-    //% y.min=1 y.max=130
-    //% weight=80
+    //% x.min=0 x.max=159
+    //% y.min=0 y.max=127
+    //% weight=90
+
     export function drawPixel(x: number, y: number, color: Color): void {
         setWindow(x, y, x + 1, y + 1)
         send(TFTCommands.RAMWR, [color >> 8, color])
     }
 
-    /*
-     * Send command to display
-     */
-    function send(command: TFTCommands, parameter: Array<number>): void {
-        // set TFT to command mode
-        pins.digitalWritePin(DigitalPin.P12, 0)
-        // select TFT controller
-        pins.digitalWritePin(DigitalPin.P16, 0)
-        // Send command
-        pins.spiWrite(command)
-        // set TFT back to data mode
-        pins.digitalWritePin(DigitalPin.P12, 1)
-        for (let item of parameter) {
-            pins.spiWrite(item)
-        }
-
-        // deselect TFT controller
-        pins.digitalWritePin(DigitalPin.P16, 1)
-    }
-
-    /*
-     * Set pixel address window - minimum and maximum pixel bounds
-     */
-    function setWindow(x0: number, y0: number, x1: number, y1: number): void {
-        x0 = (x0 & 0xFF) + 1
-        x1 = (x1 - 1 & 0xFF) + 1
-        y0 = (y0 & 0xFF) + 2
-        y1 = (y1 - 1 & 0xFF) + 2
-        send(TFTCommands.CASET, [0x00, x0, 0x00, x1])
-        send(TFTCommands.RASET, [0x00, y0, 0x00, y1])
-    }
-
-    /*
-     * Data-Mode to transfer data to TFT for further processing
-     */
-    function enterDataMode(): void {
-        // Activate command mode
-        pins.digitalWritePin(DigitalPin.P12, 0)
-        // select TFT as SPI-target
-        pins.digitalWritePin(DigitalPin.P16, 0)
-        pins.spiWrite(TFTCommands.RAMWR)
-        // Activate data mode
-        pins.digitalWritePin(DigitalPin.P12, 1)
-    }
-
-    /*
-     * Finish data-mode and set back to command-mode
-     */
-    function exitDataMode(): void {
-        pins.digitalWritePin(DigitalPin.P16, 1) // de-elect the TFT as SPI target
-        pins.digitalWritePin(DigitalPin.P12, 0) // command/data = command
-    }
-
-    /*
-    * RamRead
-    */
-    export function ram_read(addr: number, read_cnt: number = 1) {
-        let RD_Byte = []
-        pins.digitalWritePin(DigitalPin.P2, 0)   //CS ram
-        pins.spiWrite(0x3)
-        pins.spiWrite((addr >> 16) & 0xFF)
-        pins.spiWrite((addr >> 8) & 0xFF)
-        pins.spiWrite(addr & 0xFF)
-        for (let i=0;i<read_cnt;i++){
-        RD_Byte.push(pins.spiWrite(0x0))}
-        pins.digitalWritePin(DigitalPin.P2, 1) //remove ram CS
-        return RD_Byte
-         }
-   
-   
-
-    /*
-     * Draw rectangle with a given color
-     */
-    //% block="Draw rectangle at x:%x|y:%y with width:%width|height:%height|color:%color"
-    //% x.min=1 x.max=130
-    //% y.min=1 y.max=162
+    //% block="Draw horizontal line at x:%x|y:%y with length:%length and color:%color"
     //% inlineInputMode=inline
-    //% weight=70
-    export function drawRectangle(x: number, y: number, width: number, height: number, color: Color): void {
-        // Convert color
-        let hiColor = (color >> 8) % 256
-        let loColor = color % 256
+    //% weight=85
 
-        setWindow(x, y, x + width - 1, y + height - 1)
+    export function drawHorizontal(x_0: number, y_0: number, length: number, color: Color): void {
+        setWindow(x_0, y_0, x_0 + length, y_0)
+        pins.digitalWritePin(DigitalPin.P12, 1) //EN LCD data
+        pins.digitalWritePin(DigitalPin.P16, 0)  //EN LCD CS
+        for (let i = 0; i < length; i++) {
+            pins.spiWrite(color >> 8 & 0xFF)
+            pins.spiWrite(color & 0xFF)
+        }
+        pins.digitalWritePin(DigitalPin.P16, 1)  //remove LCD CS
+    }
+
+    //% block="Draw vertical line at x:%x|y:%y with length:%length and color:%color"
+    //% inlineInputMode=inline
+    //% weight=80
+
+    export function drawVertical(x_0: number, y_0: number, length: number, color: Color): void {
+        setWindow(x_0, y_0, x_0 + 1, y_0 + length)
+        pins.digitalWritePin(DigitalPin.P12, 1) //EN LCD data
+        pins.digitalWritePin(DigitalPin.P16, 0)  //EN LCD CS
+        for (let j = 0; j < length; j++) {
+            pins.spiWrite(color >> 8 & 0xFF)
+            pins.spiWrite(color & 0xFF)
+        }
+        pins.digitalWritePin(DigitalPin.P16, 1)  //remove LCD CS
+    }
+
+    /*
+    * Draw rectangle with a given color
+    */
+    //% block="Draw rectangle at x:%x|y:%y with width:%width|height:%height|color:%color"
+    //% x.min=0 x.max=159
+    //% y.min=0 y.max=127
+    //% inlineInputMode=inline
+    //% weight=75
+
+    export function drawRectangle(x_0: number, y_0: number, width: number, height: number, color: Color) {
+        let x_1 = x_0 + width
+        let y_1 = y_0 + height
+        let count = width * height
+        setWindow(x_0, y_0, x_1, y_1)
+
         enterDataMode()
 
-        // Draw rectangle
-        for (let indexY = height; indexY > 0; indexY--) {
-            for (let indexX = width; indexX > 0; indexX--) {
-                pins.spiWrite(hiColor);
-                pins.spiWrite(loColor);
-            }
+        for (let k = 0; k < count; k++) {
+            pins.spiWrite(color >> 8 & 0xFF)
+            pins.spiWrite(color & 0xFF)
         }
 
-        exitDataMode()
+        exitDataMode
     }
 
     /*
-     * Draw circle with a given radius
-     */
-    //% block="Draw circle at: x:%x|y:%y with radius:%r and color:%color"
-    //% x.min=1 x.max=162
-    //% y.min=1 y.max=130
+    * Draw a fill circle with a given radius
+    */
+    //% block="Draw fill circle at: x:%x|y:%y with radius:%r and color:%color"
+    //% x.min=0 x.max=159
+    //% y.min=0 y.max=127
     //% inlineInputMode=inline
-    //% weight=60
-    export function drawCircle(x: number, y: number, radius: number, color: Color): void {
+    //% weight=70
+
+    export function drawFillCircle(x: number, y: number, radius: number, color: Color): void {
         for (let y1 = -radius; y1 <= 0; y1++) {
             for (let x1 = -radius; x1 <= 0; x1++) {
                 if ((x1 * x1 + y1 * y1) <= (radius * radius)) {
@@ -259,40 +229,178 @@ namespace TFT {
         }
     }
 
-   
+    /*
+    * Draw circle with a given radius
+    */
+    //% block="Draw a circle at: x:%x|y:%y with radius:%r and color:%color"
+    //% x.min=0 x.max=159
+    //% y.min=0 y.max=127
+    //% inlineInputMode=inline
+    //% weight=65
 
-    //% block="Draw Character at: x0:%x0|y0:%y0 with char:%char"
-    //% weight=50
-    export function drawChar(x_0:number, y_0:number, text:string):void{
-        let list = []
-        
-        let ch = text.charCodeAt(0) - 32
-        let offset = ch * FONT_PIXEL_CNT
-     
-        list = TFT.ram_read(offset, FONT_PIXEL_CNT)
-        setWindow(x_0, y_0, x_0 + FONT_WIDTH, y_0 + FONT_HEIGHT)
-        send(TFTCommands.RAMWR, list)
-        list.splice(0, list.length)
+    export function drawCircle(center_x: number, center_y: number, radius: number, color: Color): void {
+        for (let l = 0; l < 360; l++) {
+            let rads = degreesRadians(l)
+            let x12 = center_x + Math.sin(rads) * radius
+            let y12 = center_y - Math.cos(rads) * radius
+            drawPixel(x12, y12, color)
+        }
     }
 
-    //% block="Draw Image at: x0:%x0|y0:%y0|x1:%x1|y1:%y1"
-    //% inlineInputMode=inline
-    //% weight=40
-    export function drawImage(x0:number,y0:number,x1:number,y1:number): void {
-       
-        let list = []
-        let width = (x1-x0) * 2
-        let height=y1-y0
-        let address = 0
+    /*
+    * Draw string
+    */
 
-        for (let i = 0; i < height; i++) {
-            setWindow(x0,y0+i,x1,y0+i)
-            list = TFT.ram_read(address, width)
-            send(TFTCommands.RAMWR, list)
-            list.splice(0, list.length)
-            address+=width
-        }
+    //% block="Draw string:%string at x:%x and y:%y with color:%color and background color:%bgcolor"
+    //% weight 60
     
+    export function drawString(text: string, x: number, y: number, color: Color, background: Color) {  //1
+       
+        let index = 0
+        let unicode = 0
+        let colsel = 0
+        let charIndex = 0
+
+        for (let stringPos = 0; stringPos < text.length; stringPos++) { //2
+            charIndex = text.charCodeAt(stringPos)
+
+            if (charIndex < 20) {
+                unicode = fontOne[charIndex]
+            }
+            else if (charIndex < 40) {
+                unicode = fontTwo[charIndex - 20]
+            }
+            else if (charIndex < 60) {
+                unicode = fontThree[charIndex - 40]
+            }
+            else if (charIndex < 80) {
+                unicode = fontFour[charIndex - 60]
+            }
+            else if (charIndex < 100) {
+                unicode = fontFive[charIndex - 80]
+            }
+            else if (charIndex < 120) {
+                unicode = fontSix[charIndex - 100]
+            }
+            else if (charIndex < 140) {
+                unicode = fontSeven[charIndex - 120]
+            }
+
+            let spacing = stringPos * 11
+            let xOffset = spacing + 10
+            setWindow(x + spacing, y, x + xOffset, y + 10)
+            enterDataMode()
+            for (let indexY = 0; indexY < 5; indexY++) { //3
+
+                for (let a = 0; a < 2; a++) {
+
+
+                    for (let indexX = 0; indexX < 5; indexX++) { //4
+                        index = indexY + indexX * 5
+                        colsel = (unicode & (1 << index))
+
+                        for (let b = 0; b < 2; b++) {
+                            if (colsel) {
+                                pins.spiWrite(color >> 8 & 0xFF)
+                                pins.spiWrite(color & 0xFF)
+                            }
+
+                            else {
+                                pins.spiWrite(background >> 8 & 0xFF)
+                                pins.spiWrite(background & 0xFF)
+                            }
+                        }
+                    }
+                } 
+            } 
+
+            exitDataMode()
+          
+        }
+    }
+   
+    /*
+    * Send command to display
+    */
+
+    function send(command: TFTCommands, parameter: Array<number>): void {
+        // set TFT to command-receive mode
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        // select TFT controller
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        // Send command
+        pins.spiWrite(command)
+
+        // set TFT back to data-receive mode
+        pins.digitalWritePin(DigitalPin.P12, 1)
+
+        for (let item of parameter) {
+            pins.spiWrite(item)
+        }
+
+        // deselect TFT controller
+        pins.digitalWritePin(DigitalPin.P16, 1)
+    }
+
+    /*
+    * Set pixel address window - minimum and maximum pixel bounds
+    */
+
+    function setWindow(x0: number, y0: number, x1: number, y1: number): void {
+        x0 = (x0 & 0xFF) + 1
+        x1 = ((x1 - 1) & 0xFF) + 1
+        y0 = (y0 & 0xFF) + 2
+        y1 = ((y1 - 1) & 0xFF) + 2
+        send(TFTCommands.CASET, [0x00, x0, 0x00, x1])
+        send(TFTCommands.RASET, [0x00, y0, 0x00, y1])
+        send(TFTCommands.RAMWR, [])
+    }
+
+    /*
+    * Data-Mode to transfer data to TFT for further processing
+    */
+    function enterDataMode(): void {
+        // Activate command mode
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        // CS LCD
+        pins.digitalWritePin(DigitalPin.P16, 0)
+        pins.spiWrite(TFTCommands.RAMWR)
+        // Activate data mode
+        pins.digitalWritePin(DigitalPin.P12, 1)
+    }
+
+    /*
+     * Finish data-mode and set back to command-mode
+     */
+    function exitDataMode(): void {
+        pins.digitalWritePin(DigitalPin.P16, 1) // deselect LCD
+        pins.digitalWritePin(DigitalPin.P12, 0) // command/data = command
+    }
+
+    /*
+    * RamRead
+    */
+    function ram_read(addr: number, read_cnt: number = 1) {
+        let RD_Byte = []
+        pins.digitalWritePin(DigitalPin.P2, 0)   //CS ram
+        pins.spiWrite(0x3)
+        pins.spiWrite((addr >> 16) & 0xFF)
+        pins.spiWrite((addr >> 8) & 0xFF)
+        pins.spiWrite(addr & 0xFF)
+        for (let m = 0; m < read_cnt; m++) {
+            RD_Byte.push(pins.spiWrite(0x0))
+        }
+        pins.digitalWritePin(DigitalPin.P2, 1) //remove ram CS
+        return RD_Byte
+    }
+
+    /*
+    * Convert degress to radians
+    */
+
+    function degreesRadians(degrees: number) {
+        let pi = Math.PI;
+        return degrees * (pi / 180);
     }
 
 }
